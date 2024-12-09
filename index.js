68d97cff5f9fa3c743ec7a6faf4fa66c764d7d92
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2024-12-09 16:13:54 +0000

    add rate limiting middleware

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.use((req, res, next) => {
     next();
 });
 
+
+// Rate limiting middleware
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 3, // Limit each IP to 3 
+});
+app.use(limiter);
+
 // route
 app.get('/', (req, res) => {
     res.send('Hello, World!');
