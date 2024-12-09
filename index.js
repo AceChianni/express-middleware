dc46126d19d090b4b2ca2cc1fe755e991552383d
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2024-12-09 16:05:30 +0000

    rewite code and middleware

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const app = express();
 
-// custom middleware
-app.use((req, res, next => {
-    console.log(`${req.method} request to {req.url}`);
-    next()
-}));
+// Custom middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} request to ${req.url}`);
+    next();
+});
 
 // route
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-
-
-
-// start server
+// server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
